0dcd3a30bc608006d9a228d7cecb348892a90820
PushEax <pusheax.vk@gmail.com>
2025-03-08 02:36:49 +0200

    add js observer to start animation

---
+++ src/js/covers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const refs = {
+  covers: document.querySelector('.covers'),
+  wrapper: document.querySelector('.covers-wrapper'),
+};
+
+const callback = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      refs.wrapper.classList.add('animate');
+    } else {
+      refs.wrapper.classList.remove('animate');
+    }
+  });
+};
+
+const options = {
+  root: null, // null = відстеження у всьому вікні
+  threshold: 0.1, // Спрацьовує, коли 50% елемента видно
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(refs.covers);
