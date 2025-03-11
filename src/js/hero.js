ad63a8fea159de12c1276100ed03abb7e2da6f77
MykhayloGor <ursa.mykh@gmail.com>
2025-03-11 11:43:11 +0100

    init markup and js

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,1 +1,28 @@
+const heroTitle = document.querySelector('.hero-title');
 
+function setHeroTitleMarkup(screenWidth) {
+  if (screenWidth >= 1440) {
+    return `
+      Hello.<br>
+      I'm&nbsp;Fullstack&nbsp;developer<br>
+      Lloyd Jefferson.
+    `;
+  } else {
+    return `
+      Hello.<br>
+      I'm&nbsp;Fullstack developer<br>
+      Lloyd<br>
+      Jefferson.
+    `;
+  }
+}
+
+function updateHeroTitle() {
+  const screenWidth = window.innerWidth;
+  const newMarkup = setHeroTitleMarkup(screenWidth);
+  heroTitle.innerHTML = newMarkup;
+}
+
+updateHeroTitle();
+
+window.addEventListener('resize', updateHeroTitle);
