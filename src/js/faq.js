f186430a371b14e8410dbcd67e88dfe589c94638
ishchukserhii <ischuksergei@gmail.com>
2025-03-12 00:32:04 +0200

    second

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,8 +4,7 @@ import "accordion-js/dist/accordion.min.css";
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
-  const header = item.querySelector('.accordion-header');
-  header.addEventListener('click', () => {
+  item.addEventListener('click', () => {
     item.classList.toggle('active');
   });
 });
