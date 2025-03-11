04bbfe01583beb776652a78b7cd981bff050e836
MykhayloGor <ursa.mykh@gmail.com>
2025-03-11 18:51:59 +0100

    title updated, attributes removed, animation added

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,28 +1,12 @@
-const heroTitle = document.querySelector('.hero-title');
+document.addEventListener('DOMContentLoaded', function () {
+  const heroTitle = document.querySelector('.hero-title');
+  const heroNav = document.querySelector('.hero-nav-wrapper');
 
-function setHeroTitleMarkup(screenWidth) {
-  if (screenWidth >= 1440) {
-    return `
-      Hello.<br>
-      I'm&nbsp;Fullstack&nbsp;developer<br>
-      Lloyd Jefferson.
-    `;
-  } else {
-    return `
-      Hello.<br>
-      I'm&nbsp;Fullstack developer<br>
-      Lloyd<br>
-      Jefferson.
-    `;
-  }
-}
+  setTimeout(() => {
+    heroTitle.classList.add('fade-in');
+  }, 300);
 
-function updateHeroTitle() {
-  const screenWidth = window.innerWidth;
-  const newMarkup = setHeroTitleMarkup(screenWidth);
-  heroTitle.innerHTML = newMarkup;
-}
-
-updateHeroTitle();
-
-window.addEventListener('resize', updateHeroTitle);
+  setTimeout(() => {
+    heroNav.classList.add('fade-in');
+  }, 800);
+});
