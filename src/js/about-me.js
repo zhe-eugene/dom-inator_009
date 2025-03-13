a63dbf65e3c1b8a76f12dd0d14eb2b07fa43eb6b
zhe-eugene <zhelnitskiy@gmail.com>
2025-03-13 19:55:31 +0200

    fix

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,4 +1,3 @@
-// accordion
 import Accordion from 'accordion-js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,11 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-//swiper
-
 import Swiper from 'swiper';
-//import 'swiper/swiper-bundle.css';
-//import '/css/about-me.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const swiper = new Swiper('.skills-swipe', {
