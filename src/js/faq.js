658942a102a485dc4e0f172a45b374e55095ece6
ishchukserhii <ischuksergei@gmail.com>
2025-03-13 17:26:35 +0200

    final

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,7 +4,16 @@ import Accordion from "accordion-js";
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('active');
-  });
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+
+    header.addEventListener('click', () => {
+        item.classList.toggle('active');
+
+        if (item.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.style.maxHeight = 0;
+        }
+    });
 });
